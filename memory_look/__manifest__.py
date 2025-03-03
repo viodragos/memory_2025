17abc65329db54288068ac6dd9397c58036fa724
Viorel Dragos <viodragos@gmail.com>
2025-03-03 18:02:21 +0200

    Adaugare modul personalizare aspect

---
+++ memory_look/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Memory Look',
+    'version': '1.0',
+    'category': 'Customization',
+    'summary': 'Siglă și temă albastră pentru client',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'memory_look/static/src/css/memory_look.css',
+        ],
+    },
+    'installable': True,
+    'application': True,
+}
