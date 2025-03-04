a28172f5700cc1fa4954245ca8e729115bf7b2f0
Viorel Dragos <viodragos@gmail.com>
2025-03-04 12:43:16 +0200

    Adaugare sigla firma

--- memory_look/__manifest__.py
+++ memory_look/__manifest__.py
@@ -4,11 +4,11 @@
     'category': 'Customization',
     'summary': 'Siglă și temă albastră pentru client',
     'depends': ['web'],
+    'data': ['views/header_logo.xml',],
     'assets': {
-        'web.assets_backend': [
-            'memory_look/static/src/css/memory_look.css',
-        ],
-    },
+    'web.assets_backend': [
+        '/memory_look/static/src/css/memory_look.css',
+    ],},
     'installable': True,
     'application': True,
 }
